e95833d4bb510a55dd287587ebfff3e5cbf48299
mdah <mda.hewlett@gmail.com>
2025-07-09 16:59:56 -0700

    fix: extra slash in origins on BE

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://aiqa-third.vercel.app/"],
+    allow_origins=["http://localhost:5173", "https://aiqa-third.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
